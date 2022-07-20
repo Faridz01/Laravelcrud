7aa6cd405ce18b5e9f45e23faf23834cfff6536f
unknown <fariz_241@smkassalaambandung.sch.id>
2022-07-20 11:53:28 +0700

    rungkad

---
+++ database/migrations/2022_07_20_042304_create_pembelians_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePembeliansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pembelians', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_pembeli');
+            $table->string('nama_barang');
+            $table->string('jumlah_harga');
+            $table->string('harga_satuan');
+            $table->date('tgl_pembelian');
+            $table->text('total_harga');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pembelians');
+    }
+}
